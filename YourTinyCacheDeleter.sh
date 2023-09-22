c40aa4bfcb2286862e773643dfbe66d7615b116a
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2023-09-22 10:42:34 +0900

    src commit

---
+++ YourTinyCacheDeleter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# In this script, we are going to delete all the caches in the Mac.
+# Usually, system data takes up a lot of space. 
+# Initially deleting Caches folders
+
+# Take authorization
+sudo tmutil listlocalsnapshots / 2> /dev/null;
+sudo tmutil deletelocalsnapshots / 2> /dev/null;
+
+echo "INFO: All local snapshots were deleted."
+sleep 1
+
+# Delete contents in ~/Library/Caches (use /dev/null to avoid errors)
+rm -rf ~/Library/Caches/* 2> /dev/null; # 2> /dev/null is used to avoid errors
+# Delete contents in /Library/Caches
+rm -rf /Library/Caches/* 2> /dev/null; # 2> /dev/null is used to avoid errors
+
+echo "INFO: All caches were deleted."
+sleep 1
+
+# Ask user to reboot the Mac
+echo "INFO: Do you want to reboot the Mac now? (y/n)"
+read answer
+if [ "$answer" == "y" ]; then
+    sudo reboot
+fi
